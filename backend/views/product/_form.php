9dfc124fbee3cbe83ee251bfd5e89981cf6d8f6a
lookaround3 <darkodrljaca@gmail.com>
2021-01-04 20:20:24 +0100

    Install yii2-ckeditor extension and customized product form

--- backend/views/product/_form.php
+++ backend/views/product/_form.php
@@ -4,6 +4,10 @@ use yii\helpers\Html;
 // use yii\widgets\ActiveForm;
 use yii\bootstrap4\ActiveForm;
 
+// instalirana malo starija verzija (2.1.0) zbog nekih zavisnosti koje su nepotrebne
+// php composer.phar require 2amigos/yii2-ckeditor-widget:2.1.0
+use dosamigos\ckeditor\CKEditor;
+
 
 /* @var $this yii\web\View */
 /* @var $model common\models\Product */
@@ -16,13 +20,30 @@ use yii\bootstrap4\ActiveForm;
 
     <?= $form->field($model, 'name')->textInput(['maxlength' => true]) ?>
 
-    <?= $form->field($model, 'description')->textarea(['rows' => 6]) ?>
+    <?= $form->field($model, 'description')->widget(CKEditor::className(),[
+        'options' => ['rows' => 6],
+        'preset' => 'basic'
+    ]) ?>    
 
-    <?= $form->field($model, 'image')->textInput(['maxlength' => true]) ?>
+    <?= $form->field($model, 'image', [
+        'template' => 
+          '
+            <div class="custom-file">
+              {input} 
+              {label} 
+              {error}             
+            </div>
+          ',
+        'inputOptions' => ['class' => 'custom-file-input'],
+        'labelOptions' => ['class' => 'custom-file-label']
+    ])->fileInput() ?>
 
-    <?= $form->field($model, 'price')->textInput(['maxlength' => true]) ?>
+    <?= $form->field($model, 'price')->textInput([
+        'maxlength' => true,
+        'type' => 'number'
+        ]) ?>
 
-    <?= $form->field($model, 'status')->textInput() ?>
+    <?= $form->field($model, 'status')->checkbox() ?>
 
     <div class="form-group">
         <?= Html::submitButton('Save', ['class' => 'btn btn-success']) ?>
