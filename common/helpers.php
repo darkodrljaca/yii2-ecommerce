43aee3a77072cd076344b2c9299fd0d2109e8aaa
lookaround3 <darkodrljaca@gmail.com>
2021-01-20 13:11:01 +0100

    Create param function in helpers

--- common/helpers.php
+++ common/helpers.php
@@ -1,9 +1,13 @@
 <?php
 
-function isGuest() {   
-    return Yii::$app->user->isGuest;    
+function isGuest() {
+    return Yii::$app->user->isGuest;
 }
 
 function currUserId() {
     return Yii::$app->user->id;
+}
+
+function param($key) {
+  return Yii::$app->params[$key];
 }
